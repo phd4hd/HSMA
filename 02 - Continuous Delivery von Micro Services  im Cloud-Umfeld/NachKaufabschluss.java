fde1addfeed5ecdbbfa6a9fef735719e85af587d
Dr. Peter Dillinger <104060003+phd4hsma@users.noreply.github.com>
2023-05-07 17:40:16 +0200

    Eine einfache JAVA-Quelldatei

---
+++ 02 - Continuous Delivery von Micro Services  im Cloud-Umfeld/NachKaufabschluss.java
@@ -0,0 +1,6 @@
+public class NachKaufabschluss {
+	public static void main(String[] args) {
+		System.out.println("Vielen Dank für Ihren Einkauf!");
+		System.out.println("Vielleicht können wir Ihnen weitere Produkte empfehlen?");
+	}
+}
